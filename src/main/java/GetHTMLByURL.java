a9213e53bbd0e64783167bdd71c8c1780f20ce0a
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-09-25 13:06:27 +0800

    Create GetHTMLByURL.java

---
+++ src/main/java/GetHTMLByURL.java
@@ -0,0 +1,50 @@
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.HttpClientUtils;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+/**
+ * @author XUAN
+ * @date 2020/9/24 - 21:37
+ * @references [Java爬虫系列二：使用HttpClient抓取页面HTML](https://www.cnblogs.com/sam-uncle/p/10908567.html)
+ * @purpose 输入 URL,获取其 HTML 页面
+ */
+public class GetHTMLByURL {
+
+    public static String getHTML(String url){
+        String html=null;
+        //1.生成httpclient，相当于该打开一个浏览器
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        CloseableHttpResponse response = null;
+        try {
+            //2.创建get请求，相当于在浏览器地址栏输入网址
+            HttpGet request = new HttpGet(url);
+            //3.执行get请求，相当于在输入地址栏后敲回车键
+            response = httpClient.execute(request);
+            //4.判断响应状态为200，进行处理
+            if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                //5.获取响应内容
+                HttpEntity httpEntity = response.getEntity();
+                html = EntityUtils.toString(httpEntity, "utf-8");
+            } else {
+                //如果返回状态不是200，比如404（页面不存在）等，根据情况做处理，这里略
+                System.out.println("返回状态是:" + response.getStatusLine().getStatusCode());
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //6.关闭
+            HttpClientUtils.closeQuietly(response);
+            HttpClientUtils.closeQuietly(httpClient);
+        }
+        return html;
+    }
+}
